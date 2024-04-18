c6a0e20202645cbf57f2424a8bb1fe4b23abbf49
Shruthi <shruthimohan.professional@gmail.com>
2024-04-18 11:14:56 +0530

    Initial commit

---
+++ springboot-kafka-tutorial/src/test/java/com/indium/springbootkafkatutorial/SpringbootKafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indium.springbootkafkatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
